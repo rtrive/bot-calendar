2c021ffeea9c5e9ed2360712e6a7e622aefffcc5
Riccardo Trivellato <riccardo.trivellato@mollie.com>
2023-06-06 09:49:20 +0200

    feat: bot init

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	telegramBotApiKey := os.Getenv("TELEGRAM_BOT_API_KEY")
+
+	bot, err := tgbotapi.NewBotAPI(telegramBotApiKey)
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 }
