f5c531b0413eb8fac6b571015ac87dc134f19bac
Riccardo Trivellato <riccardo.trivellato@mollie.com>
2023-06-16 15:31:57 +0200

    feat: calendar refactor

---
+++ calendar.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+const primaryCalendar = "primary"
+
+func getTodayEvent(srv *calendar.Service) []*calendar.Event {
+	t := time.Now()
+
+	todayMidnight := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Format(time.RFC3339)
+	todayEnd := time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 99, t.Location()).Format(time.RFC3339)
+
+	todayEvents, _ := srv.Events.List(primaryCalendar).TimeMin(todayMidnight).TimeMax(todayEnd).ShowDeleted(false).SingleEvents(true).Do()
+	return todayEvents.Items
+}
+
+func initCalendar(ctx context.Context, client *http.Client) *calendar.Service {
+	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+	return srv
+}
--- main.go
+++ main.go
@@ -5,24 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
-	"google.golang.org/api/calendar/v3"
-	"google.golang.org/api/option"
 )
-
-const primaryCalendar = "primary"
-
-func getTodayEvent(srv *calendar.Service) []*calendar.Event {
-	t := time.Now()
-
-	todayMidnight := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Format(time.RFC3339)
-	todayEnd := time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 99, t.Location()).Format(time.RFC3339)
-
-	todayEvents, _ := srv.Events.List(primaryCalendar).TimeMin(todayMidnight).TimeMax(todayEnd).ShowDeleted(false).SingleEvents(true).Do()
-	return todayEvents.Items
-}
 
 func checkEnv(name string) string {
 	err := godotenv.Load()
@@ -36,11 +21,7 @@ func checkEnv(name string) string {
 func main() {
 	ctx := context.Background()
 	client := initOauth()
-
-	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		log.Fatalf("Unable to retrieve Calendar client: %v", err)
-	}
+	srv := initCalendar(ctx, client)
 
 	todayEvents := getTodayEvent(srv)
 	for _, event := range todayEvents {
