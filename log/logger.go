52feae0f28182f5f5a9a8b154e2961e10b1b83f7
Riccardo Trivellato <riccardo.trivellato@mollie.com>
2023-06-19 15:00:26 +0200

    feat: add log level

--- log/logger.go
+++ log/logger.go
@@ -1,11 +1,26 @@
 package log
 
 import (
+	u "github.com/rtrive/bot-calendar/utility"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var log *zap.SugaredLogger
+
+func logLevel() zapcore.Level {
+	level := u.CheckEnv("LOG_LEVEL")
+	switch level {
+	case "DEBUG":
+		return zap.DebugLevel
+	case "INFO":
+		return zap.InfoLevel
+	case "ERROR":
+		return zap.ErrorLevel
+	default:
+		return zap.InfoLevel
+	}
+}
 
 func init() {
 
@@ -19,6 +34,7 @@ func init() {
 		EncodeCaller: zapcore.ShortCallerEncoder,
 	}
 	config := zap.NewProductionConfig()
+	config.Level.SetLevel(logLevel())
 	config.EncoderConfig = cfgEncoderConfig
 	tmpLog, _ := config.Build(zap.AddCallerSkip(1))
 	log = tmpLog.Sugar()
