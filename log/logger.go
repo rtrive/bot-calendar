eb61360918d8e0f49230ef644b1bb98b59596b80
Riccardo Trivellato <riccardo.trivellato@mollie.com>
2023-06-19 12:40:57 +0200

    chore: replace json config

--- log/logger.go
+++ log/logger.go
@@ -1,34 +1,26 @@
 package log
 
 import (
-	"encoding/json"
-
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var log *zap.SugaredLogger
 
 func init() {
 
-	rawJSON := []byte(`{
-		"level": "debug",
-		"encoding": "json",
-		"outputPaths": ["stdout", "/tmp/logs"],
-		"errorOutputPaths": ["stderr"],
-		"encoderConfig": {
-		  "messageKey": "message",
-		  "levelKey": "level",
-		  "levelEncoder": "lowercase"
-		}
-	  }`)
-	var cfg zap.Config
-	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
-		panic(err)
+	cfgEncoderConfig := zapcore.EncoderConfig{
+		MessageKey:  "message",
+		LevelKey:    "level",
+		EncodeLevel: zapcore.LowercaseLevelEncoder,
+		TimeKey:     "timestamp",
+		EncodeTime:  zapcore.RFC3339TimeEncoder,
 	}
-	logger := zap.Must(cfg.Build())
-	defer logger.Sync()
+	config := zap.NewProductionConfig()
+	config.EncoderConfig = cfgEncoderConfig
+	tmpLog, _ := config.Build(zap.AddCallerSkip(1))
+	log = tmpLog.Sugar()
 
-	log = logger.Sugar()
 }
 
 func Info(message string) {
